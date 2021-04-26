456ba7ddb56e839fd1dd57599fd5d9614bb393ca
sajalgupta06 <57083599+sajalgupta06@users.noreply.github.com>
2021-04-26 12:32:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello there 
+          Learn React 
         </a>
       </header>
     </div>
